cfa1996a2dccd365fb2995378c76755e2cdd1c04
Epimetheus012 <epimetheus012@Epimetheus012deMacBook-Air.local>
2025-07-15 13:53:21 +0800

    build/ deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/scent-personality-quiz/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: '/public/index.html' // 显式指定入口
+    }
+  }
 });
