354af7b53c01a604f0d70cfc1b2c887f2c892219
Epimetheus012 <epimetheus012@Epimetheus012deMacBook-Air.local>
2025-12-29 20:50:06 -0800

    update style

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: '/scent-personality-quiz/',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
       input: 'index.html' // 显式指定入口
